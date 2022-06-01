ec23ca77ffe53bb7cd894e8d909ecd307ab8d652
barf <bar.friedman2@mail.huji.ac.il>
2022-06-01 23:30:33 +0200

    data split

--- data_visualization.py
+++ data_visualization.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GroupShuffleSplit
 
 
 def load_data():
-    X = pd.read_csv("train.feats.csv")
+    X = pd.read_csv("train.feats.csv", dtype={'אבחנה-Surgery name3': 'str', "אבחנה-Ivi -Lymphovascular invasion": 'str',
+                                              'אבחנה-Surgery date3': 'str'})
     y = pd.read_csv("train.labels.0.csv")
-    # X.groupby()
-    x_train, x_test, y_train, y_test = train_test_split(X,y,train_size=10000)
-    pd.DataFrame(x_train)
-    print(x_train)
 
+    # split data:
+    splitter = GroupShuffleSplit(test_size=.80, n_splits=2, random_state=7)
+    split = splitter.split(X, groups=X['User Name'])
+    train_inds, test_inds = next(split)
 
-
+    train_x = X.iloc[train_inds]
+    test_x = X.iloc[test_inds]
+    train_y = y.iloc[train_inds]
+    test_y = y.iloc[test_inds]
 
 if __name__ == '__main__':
     # np.random.seed(0)
