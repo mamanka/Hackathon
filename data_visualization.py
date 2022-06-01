ccfc06fd004a5193af4843cd702face078a68cc8
mamanka <uria.maman@mail.huji.ac.il>
2022-06-01 22:33:16 +0300

    "current 22:33"

--- data_visualization.py
+++ data_visualization.py
@@ -4,10 +4,10 @@ from sklearn.model_selection import train_test_split
 
 
 def load_data():
-    X = pd.read_csv("train.feats.csv")
-    y = pd.read.csv("train.labels.0.csv")
-    j = 5
-    train_X, train_y, test_X, test_y = train_test_split(pd.DataFrame(X), pd.Series(y), train_proportion=2 / 3)
+    X = pd.read_csv("train.feats.csv", dtype={'אבחנה-Surgery name3': 'str', "אבחנה-Ivi -Lymphovascular invasion": 'str',
+                                              'אבחנה-Surgery date3': 'str'})
+    y = pd.read_csv("train.labels.0.csv")
+    train_X, train_y, test_X, test_y = train_test_split(X.to_numpy(), y.to_numpy(), train_size=10000)
 
 
 if __name__ == '__main__':
