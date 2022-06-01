5ad1c46ec09c6b9c9595d8f04977d47235ef081b
mamanka <uria.maman@mail.huji.ac.il>
2022-06-01 22:05:40 +0300

    "uria files"

--- data_visualization.py
+++ data_visualization.py
@@ -2,22 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+
 def load_data():
     X = pd.read_csv("train.feats.csv")
-    y = pd.read.csv("train.labels.csv")
+    y = pd.read.csv("train.labels.0.csv")
     train_X, train_y, test_X, test_y = train_test_split(pd.DataFrame(X), pd.Series(y), train_proportion=2 / 3)
-
-
-
-
-
 
 
 if __name__ == '__main__':
     # np.random.seed(0)
     load_data()
-
-
-
 
     # Question 1 - Load and preprocessing of city temperature dataset
